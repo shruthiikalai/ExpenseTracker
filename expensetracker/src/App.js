b095b086e72b5280b7af7b42ca6977a967010dc1
Kalaiyarasi Rajendran <kalaiyarasirajendran@healthifydigital.com>
2024-11-11 13:11:44 +0530

    Initial commit

---
+++ expensetracker/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './index.css';
+import Container from './Container';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container />
+      </header>
+    </div>
+  );
+}
+
+export default App;
